e183a82ba4f3cea9a11adef07623200e0b07a9f3
imad <147118339+eddineimad@users.noreply.github.com>
2023-11-21 02:50:38 +0100

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,12 @@
+public class Employee  {
+        int number ;
+        String name ;
+        int sales ;
+        int bonushrs ;
+        int basesalary ;
+
+        void CalculateNetSalary() {
+            System.out.println("The employee number is " + number + ". His Name is " + name + ". His Base salary is " + basesalary + ". His Sales are " + sales + ". And his Bonus hours is " + bonushrs) ;
+        }
+
+}
